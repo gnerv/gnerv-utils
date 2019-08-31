85fcbbfc17d6a22d775ff2c569943ee81a3fbce0
lgnerv <ligendwow@126.com>
2019-08-31 18:42:19 +0800

    inti v1

---
+++ src/main/java/com/gnerv/boot/utils/ExcelUtil.java
@@ -0,0 +1,143 @@
+package com.gnerv.boot.utils;
+
+import org.apache.poi.hssf.usermodel.HSSFWorkbook;
+import org.apache.poi.ss.usermodel.Cell;
+import org.apache.poi.ss.usermodel.CellType;
+import org.apache.poi.ss.usermodel.Row;
+import org.apache.poi.ss.usermodel.Sheet;
+import org.apache.poi.ss.usermodel.Workbook;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * @author Gnerv LiGen
+ * @description Excel工具
+ * @data 2019/6/13
+ */
+public class ExcelUtil {
+
+    private final static String xls = "xls";
+    private final static String xlsx = "xlsx";
+
+    /**
+     * 读入excel文件，解析后返回
+     *
+     * @param file
+     * @throws IOException
+     */
+    public static List<Object[]> readExcel(MultipartFile file) throws IOException {
+        //检查文件
+        checkFile(file);
+        //获得Workbook工作薄对象
+        Workbook workbook = getWorkBook(file);
+        //创建返回对象，把每行中的值作为一个数组，所有行作为一个集合返回
+        List<Object[]> list = new ArrayList<>();
+        if (workbook != null) {
+            for (int sheetNum = 0; sheetNum < workbook.getNumberOfSheets(); sheetNum++) {
+                //获得当前sheet工作表
+                Sheet sheet = workbook.getSheetAt(sheetNum);
+                if (sheet == null) {
+                    continue;
+                }
+                //获得当前sheet的开始行
+                int firstRowNum = sheet.getFirstRowNum();
+                //获得当前sheet的结束行
+                int lastRowNum = sheet.getLastRowNum();
+                //循环除了第一行的所有行
+                for (int rowNum = firstRowNum + 1; rowNum <= lastRowNum; rowNum++) {
+                    //获得当前行
+                    Row row = sheet.getRow(rowNum);
+                    if (row == null) {
+                        continue;
+                    }
+                    //获得当前行的开始列
+                    int firstCellNum = row.getFirstCellNum();
+                    //获得当前行的列数
+                    int lastCellNum = row.getPhysicalNumberOfCells();
+                    Object[] cells = new String[row.getPhysicalNumberOfCells()];
+                    //循环当前行
+                    for (int cellNum = firstCellNum; cellNum < lastCellNum; cellNum++) {
+                        Cell cell = row.getCell(cellNum);
+                        cells[cellNum] = getCellValue(cell);
+                    }
+                    list.add(cells);
+                }
+            }
+            workbook.close();
+        }
+        return list;
+    }
+
+    public static void checkFile(MultipartFile file) throws IOException {
+        //判断文件是否存在
+        if (null == file) {
+            throw new FileNotFoundException("文件不存在！");
+        }
+        //获得文件名
+        String fileName = file.getOriginalFilename();
+        //判断文件是否是excel文件
+        if (!fileName.endsWith(xls) && !fileName.endsWith(xlsx)) {
+            throw new IOException(fileName + "不是excel文件");
+        }
+    }
+
+    public static Workbook getWorkBook(MultipartFile file) {
+        //获得文件名
+        String fileName = file.getOriginalFilename();
+        //创建Workbook工作薄对象，表示整个excel
+        Workbook workbook = null;
+        try {
+            //获取excel文件的io流
+            InputStream is = file.getInputStream();
+            //根据文件后缀名不同(xls和xlsx)获得不同的Workbook实现类对象
+            if (fileName.endsWith(xls)) {
+                //2003
+                workbook = new HSSFWorkbook(is);
+            } else if (fileName.endsWith(xlsx)) {
+                //2007
+                workbook = new XSSFWorkbook(is);
+                new XSSFWorkbook();
+            }
+        } catch (IOException e) {
+        }
+        return workbook;
+    }
+
+    public static String getCellValue(Cell cell) {
+        String cellValue = "";
+        if (cell == null) {
+            return cellValue;
+        }
+        //把数字当成String来读，避免出现1读成1.0的情况
+        if (cell.getCellType() == CellType.NUMERIC) {
+            cell.setCellType(CellType.STRING);
+        }
+        //判断数据的类型
+        CellType cellType = cell.getCellType();
+        if (cellType.equals(CellType.NUMERIC)) {
+            cellValue = String.valueOf(cell.getNumericCellValue());
+        }
+        if (cellType.equals(CellType.STRING)) {
+            cellValue = String.valueOf(cell.getStringCellValue());
+        }
+        if (cellType.equals(CellType.BOOLEAN)) {
+            cellValue = String.valueOf(cell.getBooleanCellValue());
+        }
+        if (cellType.equals(CellType.FORMULA)) {
+            cellValue = String.valueOf(cell.getCellFormula());
+        }
+        if (cellType.equals(CellType.BLANK)) {
+            cellValue = "";
+        }
+        if (cellType.equals(CellType.ERROR)) {
+            cellValue = "非法字符";
+        }
+        return cellValue;
+    }
+}
